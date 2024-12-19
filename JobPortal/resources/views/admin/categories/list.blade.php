e133960fa17881d23c6f22b55c37a1f2ab26021c
Agarwal7Tarun <atarun21@tbc.edu.np>
2024-12-19 16:48:01 +0545

    Added Function to Edit Job Category Status and Add New Categories to Admin Panel

--- JobPortal/resources/views/admin/categories/list.blade.php
+++ JobPortal/resources/views/admin/categories/list.blade.php
@@ -1,13 +1,13 @@
+@extends('front.layouts.app')
 
-
-<?php $__env->startSection('body'); ?>
+@section('body')
 <section class="section-5 bg-2">
     <div class="container py-5">
         <div class="row">
             <div class="col">
                 <nav aria-label="breadcrumb" class=" rounded-3 p-3 mb-4">
                     <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="<?php echo e(route('admin.dashboard')); ?>">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
                         <li class="breadcrumb-item active">Categories</li>
                     </ol>
                 </nav>
@@ -15,10 +15,10 @@
         </div>
         <div class="row">
             <div class="col-lg-3">
-                <?php echo $__env->make('admin.sidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                @include('admin.sidebar')
             </div>
             <div class="col-lg-9">
-                <?php echo $__env->make('front.message', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                @include('front.message')
                 <div class="card border-0 shadow mb-4">
                     <div class="card-body card-form">
                         <div class="d-flex justify-content-between">
@@ -26,7 +26,7 @@
                                 <h3 class="fs-4 mb-1">Categories</h3>
                             </div>
                             <div style="margin-top: -10px;">
-                                <a href="<?php echo e(route('admin.categories.create')); ?>" class="btn btn-primary">Create</a>
+                                <a href="{{ route('admin.categories.create') }}" class="btn btn-primary">Create</a>
                             </div>
                         </div>
                         <div class="table-responsive">
@@ -40,19 +40,19 @@
                                     </tr>
                                 </thead>
                                 <tbody class="border-0">
-                                    <?php if($categories->isNotEmpty()): ?>
-                                        <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    @if ($categories->isNotEmpty())
+                                        @foreach ($categories as $category)
                                         <tr class="active">
-                                            <td><?php echo e($category->id); ?></td>
+                                            <td>{{ $category->id }}</td>
                                             <td>
-                                                <div class="job-name fw-500"><?php echo e($category->name); ?></div>
+                                                <div class="job-name fw-500">{{ $category->name }}</div>
                                             </td>
                                             <td>
-                                                <?php if($category->status == 1): ?>
+                                                @if ($category->status == 1)
                                                     <p class="text-success">Active</p>
-                                                <?php else: ?>
+                                                @else
                                                     <p class="text-danger">Not Active</p>
-                                                <?php endif; ?>
+                                                @endif
                                             </td>
                                             <td>
                                                 <div class="action-dots">
@@ -60,25 +60,24 @@
                                                         <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
                                                     </button>
                                                     <ul class="dropdown-menu dropdown-menu-end">
-                                                        <li><a class="dropdown-item" href="<?php echo e(route('admin.categories.edit',$category->id)); ?>"><i class="fa fa-edit" aria-hidden="true"></i> Edit</a></li>
-                                                        <li><a class="dropdown-item" href="javascript:void(0)" onclick="deleteCategory(<?php echo e($category->id); ?>)"><i class="fa fa-trash" aria-hidden="true"></i> Remove</a></li>
+                                                        <li><a class="dropdown-item" href="{{ route('admin.categories.edit',$category->id) }}"><i class="fa fa-edit" aria-hidden="true"></i> Edit</a></li>
+                                                        <li><a class="dropdown-item" href="javascript:void(0)" onclick="deleteCategory({{ $category->id }})"><i class="fa fa-trash" aria-hidden="true"></i> Remove</a></li>
                                                     </ul>
                                                 </div>
                                             </td>
                                         </tr>
-                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                                    <?php else: ?>
+                                    @endforeach
+                                    @else
                                         <tr>
                                             <td colspan="6">Users not Found</td>
                                         </tr>
-                                    <?php endif; ?>
+                                    @endif
                                 </tbody>
 
                             </table>
                         </div>
                         <div>
-                            <?php echo e($categories->links()); ?>
-
+                            {{ $categories->links() }}
                         </div>
                     </div>
                 </div>
@@ -88,25 +87,23 @@
     </div>
 </section>
 
-<?php $__env->stopSection(); ?>
+@endsection
 
-<?php $__env->startSection('customJs'); ?>
+@section('customJs')
     <script type="text/javascript">
 
         function deleteCategory(id){
             if(confirm('Are you sure you want to delete?')){
                 $.ajax({
                     type: "delete",
-                    url: "<?php echo e(route('admin.categories.destroy')); ?>",
+                    url: "{{ route('admin.categories.destroy') }}",
                     data: {id: id},
                     dataType: "json",
                     success: function (response) {
-                        window.location.href="<?php echo e(route('admin.categories')); ?>";
+                        window.location.href="{{ route('admin.categories') }}";
                     }
                 });
             }
         }
     </script>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('front.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\component-2-Agarwal7Tarun\JobPortal\resources\views/admin/categories/list.blade.php ENDPATH**/ ?>
+@endsection
