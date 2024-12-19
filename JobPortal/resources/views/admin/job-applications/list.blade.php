a7e4b843bd6874556ebc6194eb0b511034f66c70
Agarwal7Tarun <atarun21@tbc.edu.np>
2024-12-19 16:44:02 +0545

    Added View and Edit for Job Applications in Admin Panel

--- JobPortal/resources/views/admin/job-applications/list.blade.php
+++ JobPortal/resources/views/admin/job-applications/list.blade.php
@@ -1,13 +1,13 @@
+@extends('front.layouts.app')
 
-
-<?php $__env->startSection('body'); ?>
+@section('body')
 <section class="section-5 bg-2">
     <div class="container py-5">
         <div class="row">
             <div class="col">
                 <nav aria-label="breadcrumb" class=" rounded-3 p-3 mb-4">
                     <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="<?php echo e(route('admin.dashboard')); ?>">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
                         <li class="breadcrumb-item active">Job Applications</li>
                     </ol>
                 </nav>
@@ -15,10 +15,10 @@
         </div>
         <div class="row">
             <div class="col-lg-3">
-                <?php echo $__env->make('admin.sidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                @include('admin.sidebar')
             </div>
             <div class="col-lg-9">
-                <?php echo $__env->make('front.message', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                @include('front.message')
                 <div class="card border-0 shadow mb-4">
                     <div class="card-body card-form">
                         <div class="d-flex justify-content-between">
@@ -39,42 +39,41 @@
                                     </tr>
                                 </thead>
                                 <tbody class="border-0">
-                                    <?php if($jobApplications->isNotEmpty()): ?>
-                                        <?php $__currentLoopData = $jobApplications; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $jobApplication): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    @if ($jobApplications->isNotEmpty())
+                                        @foreach ($jobApplications as $jobApplication)
                                         <tr class="active">
                                             <td>
-                                                <div class="job-name fw-500"><?php echo e($jobApplication->job->title); ?></div>
-                                                
+                                                <div class="job-name fw-500">{{ $jobApplication->job->title }}</div>
+                                                {{-- <div class="info1">{{ $jobApplication->job->jobType->name }} . {{ $jobApplication->job->location }}</div> --}}
                                             </td>
-                                            <td><?php echo e($jobApplication->user->name); ?></td>
-                                            <td><?php echo e($jobApplication->employer->name); ?></td>
-                                            <td><?php echo e(\Carbon\Carbon::parse($jobApplication->applied_date )->format('d M, Y')); ?></td>
+                                            <td>{{ $jobApplication->user->name }}</td>
+                                            <td>{{ $jobApplication->employer->name }}</td>
+                                            <td>{{ \Carbon\Carbon::parse($jobApplication->applied_date )->format('d M, Y') }}</td>
                                             <td>
                                                 <div class="action-dots">
                                                     <button href="#" class="btn" data-bs-toggle="dropdown" aria-expanded="false">
                                                         <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
                                                     </button>
                                                     <ul class="dropdown-menu dropdown-menu-end">
-                                                        <li><a class="dropdown-item" href="<?php echo e(route('jobDetail',$jobApplication->job_id)); ?>"> <i class="fa fa-eye" aria-hidden="true"></i> View</a></li>
-                                                        <li><a class="dropdown-item" href="javascript:void(0)" onclick="removeJob(<?php echo e($jobApplication->id); ?>)"><i class="fa fa-trash" aria-hidden="true"></i> Remove</a></li>
+                                                        <li><a class="dropdown-item" href="{{ route('jobDetail',$jobApplication->job_id) }}"> <i class="fa fa-eye" aria-hidden="true"></i> View</a></li>
+                                                        <li><a class="dropdown-item" href="javascript:void(0)" onclick="removeJob({{ $jobApplication->id }})"><i class="fa fa-trash" aria-hidden="true"></i> Remove</a></li>
                                                     </ul>
                                                 </div>
                                             </td>
                                         </tr>
-                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                                    <?php else: ?>
+                                    @endforeach
+                                    @else
                                         <tr>
                                             <td colspan="5">Job Applications not found</td>
                                         </tr>
-                                    <?php endif; ?>
+                                    @endif
 
                                 </tbody>
 
                             </table>
                         </div>
                         <div>
-                            <?php echo e($jobApplications->links()); ?>
-
+                            {{ $jobApplications->links() }}
                         </div>
                     </div>
                 </div>
@@ -84,26 +83,24 @@
     </div>
 </section>
 
-<?php $__env->stopSection(); ?>
+@endsection
 
-<?php $__env->startSection('customJs'); ?>
+@section('customJs')
     <script type="text/javascript">
         function removeJob(job_id){
             if(confirm('Are you sure you want to remove?')){
                 $.ajax({
                     type: "delete",
-                    url: "<?php echo e(route('admin.jobApplications.destroy')); ?>",
+                    url: "{{ route('admin.jobApplications.destroy') }}",
                     data: {job_id},
                     dataType: "json",
                     success: function (response) {
                         if(response.status == true){
-                            window.location.href="<?php echo e(route('admin.jobApplications')); ?>";
+                            window.location.href="{{ route('admin.jobApplications') }}";
                         }
                     }
                 });
             }
         }
     </script>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('front.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\component-2-Agarwal7Tarun\JobPortal\resources\views/admin/job-applications/list.blade.php ENDPATH**/ ?>
+@endsection
